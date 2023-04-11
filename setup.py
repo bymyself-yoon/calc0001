a77a1d6c9fc2553648bc5780d6593663e57db24d
bymyself-yoon <120462702+bymyself-yoon@users.noreply.github.com>
2023-04-11 12:44:55 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup (
+    name = 'bymyself_yoon',
+    version = '0.0.1',
+    description = 'nice calculator',
+    author = 'suyun',
+    url = 'https://github.com/bymyself-yoon/calc0001',
+    download_url = 'https://github.com/bymyself-yoon/calc0001',
+    packages = [ 'myclac' ],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+    ]
+)
